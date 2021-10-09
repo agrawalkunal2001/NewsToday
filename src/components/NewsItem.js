7a0f552d4c5c957b9da601e10e91b719b916bb8c
Plane <plane2001@outlook.com>
2021-10-09 16:25:42 +0530

    Showed using fetch api

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,8 +9,8 @@ export default class NewsItem extends Component {
                 <div className="card" style={{width: "18rem"}}/* One bracket for js and other for an object*/>
                     <img src={imageUrl} className="card-img-top" alt="..."/>
                     <div class ="card-body">
-                    <h5 class ="card-title">{title}</h5>
-                    <p class ="card-text">{description}</p>
+                    <h5 class ="card-title">{title}...</h5>
+                    <p class ="card-text">{description}...</p>
                     <a href={newsUrl} target="_blank" /* target="_blank" opens news in a new tab*/ class ="btn btn-primary">Read More</a>
                     </div>
                 </div>
